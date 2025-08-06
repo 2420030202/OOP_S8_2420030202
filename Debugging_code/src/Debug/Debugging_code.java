81b4858421fc7594c889fd7f9c9320c466b12418
2420030202 <2420030202@klh.edu.in>
2025-08-06 10:26:54 +0530

    Add files via upload

---
+++ Debugging_code/src/Debug/Debugging_code.java
@@ -0,0 +1,10 @@
+package Debug;
+public class Debugging_code {
+	public static void main(String[] args) {
+		  String name = "  rAjEsh ";
+	        String trim=name.trim();
+	        trim=trim.toLowerCase();
+	        trim=trim.replace("a", "@");
+	        System.out.println("Formatted Name: " + trim);
+	}
+}
